31dc4c1ceb08ffe43aa1a2534a16b7a8ecf87fb7
hieulmhe176232 <hieulmhe176232@fpt.edu.vn>
2025-10-02 23:41:51 +0700

    fix: sort room pricing

--- src/main/java/com/capstone/capstone/service/impl/RoomPricingService.java
+++ src/main/java/com/capstone/capstone/service/impl/RoomPricingService.java
@@ -4,6 +4,7 @@ import com.capstone.capstone.dto.response.room.RoomPricingResponse;
 import com.capstone.capstone.repository.RoomPricingRepository;
 import com.capstone.capstone.service.interfaces.IRoomPricingService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,7 +15,7 @@ public class RoomPricingService implements IRoomPricingService {
     private final RoomPricingRepository roomPricingRepository;
 
     public List<RoomPricingResponse> getAllRoomPricing() {
-        return roomPricingRepository.findAll().stream().map(pricing -> RoomPricingResponse.builder()
+        return roomPricingRepository.findAll(Sort.by(Sort.Direction.ASC, "price")).stream().map(pricing -> RoomPricingResponse.builder()
                 .price(pricing.getPrice())
                 .id(pricing.getId())
                 .totalSlot(pricing.getTotalSlot())
