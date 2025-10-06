f596e6d255dfc1b893cbf0b0496ad3d2bcc2e60b
hieulmhe176232 <hieulmhe176232@fpt.edu.vn>
2025-10-06 22:31:30 +0700

    feat: semester service

---
+++ src/main/java/com/capstone/capstone/service/impl/SemesterService.java
@@ -0,0 +1,16 @@
+package com.capstone.capstone.service.impl;
+
+import com.capstone.capstone.entity.Semester;
+import com.capstone.capstone.repository.SemesterRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class SemesterService {
+    private final SemesterRepository semesterRepository;
+
+    public Semester getNextSemester() {
+        return semesterRepository.findNextSemester();
+    }
+}
