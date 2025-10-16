5a50c5836f8edebee065729b3875e877becdef1d
hieulmhe176232 <hieulmhe176232@fpt.edu.vn>
2025-10-16 17:40:53 +0700

    refactor: remove unused interface

--- src/main/java/com/capstone/capstone/service/interfaces/IRoomPricingService.java
+++
@@ -1,9 +0,0 @@
-package com.capstone.capstone.service.interfaces;
-
-import com.capstone.capstone.dto.response.room.RoomPricingResponse;
-
-import java.util.List;
-
-public interface IRoomPricingService {
-    List<RoomPricingResponse> getAllRoomPricing();
-}
