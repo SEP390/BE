dc430fd42362b66b78a2e6583487f07689a6f913
DuongNT21 <ngotungduong121@gmail.com>
2025-10-19 08:49:25 +0700

    Get all request (raw)

--- src/main/java/com/capstone/capstone/service/interfaces/IRequestService.java
+++ src/main/java/com/capstone/capstone/service/interfaces/IRequestService.java
@@ -3,15 +3,18 @@ package com.capstone.capstone.service.interfaces;
 import com.capstone.capstone.dto.request.request.CreateRequestRequest;
 import com.capstone.capstone.dto.request.request.UpdateRequestRequest;
 import com.capstone.capstone.dto.response.request.CreateRequestResponse;
+import com.capstone.capstone.dto.response.request.GetAllRequestResponse;
 import com.capstone.capstone.dto.response.request.GetRequestByIdResponse;
 import com.capstone.capstone.dto.response.request.UpdateRequestResponse;
+import com.capstone.capstone.dto.response.surveyQuestion.GetAllQuestionResponse;
 import com.capstone.capstone.entity.Request;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface IRequestService {
     CreateRequestResponse createRequest(CreateRequestRequest request);
     UpdateRequestResponse updateRequest(UpdateRequestRequest request, UUID id);
     GetRequestByIdResponse getRequestById(UUID id);
-
+    List<GetAllRequestResponse> getAllRequest();
 }
