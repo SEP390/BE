d26da105a3d778c8d63a6bc20f1fe747b58d39dc
DuongNT21 <ngotungduong121@gmail.com>
2025-11-20 23:55:01 +0700

    add request type

--- src/main/java/com/capstone/capstone/dto/enums/RequestTypeEnum.java
+++ src/main/java/com/capstone/capstone/dto/enums/RequestTypeEnum.java
@@ -6,6 +6,7 @@ public enum RequestTypeEnum {
     SECURITY_INCIDENT,              // Sự cố an ninh (đánh nhau, mất trộm, nghi vấn…)
     TECHNICAL_ISSUE,                // Sự cố kỹ thuật (hỏng điện/nước/thiết bị)
     POLICY_VIOLATION_REPORT,
+    CHANGEROOM,
     ANONYMOUSE,// Báo cáo vi phạm quy định (report người khác)
     OTHER                           // Khác
 }
