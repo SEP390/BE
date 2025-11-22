c8228fff0e1c2b81d3da7b3d5c9f8c9db7d1e8f1
DuongNT21 <ngotungduong121@gmail.com>
2025-11-23 01:31:57 +0700

    Add get all warehouse transaction

--- src/main/java/com/capstone/capstone/repository/WarehouseTransactionRepository.java
+++ src/main/java/com/capstone/capstone/repository/WarehouseTransactionRepository.java
@@ -1,9 +1,12 @@
 package com.capstone.capstone.repository;
 
+import com.capstone.capstone.entity.User;
 import com.capstone.capstone.entity.WarehouseTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface WarehouseTransactionRepository extends JpaRepository<WarehouseTransaction, UUID> {
+    List<WarehouseTransaction> findAllByUser(User user);
 }
--- src/main/java/com/capstone/capstone/service/impl/WareHouseTransactionService.java
+++ src/main/java/com/capstone/capstone/service/impl/WareHouseTransactionService.java
@@ -4,22 +4,21 @@ import com.capstone.capstone.dto.enums.RoleEnum;
 import com.capstone.capstone.dto.enums.TransactionTypeEnum;
 import com.capstone.capstone.dto.request.warehouseTransaction.CreateWarehouseTransactionRequest;
 import com.capstone.capstone.dto.response.warehouseTransaction.CreateWarehouseTransactionResponse;
-import com.capstone.capstone.entity.Employee;
+import com.capstone.capstone.dto.response.warehouseTransaction.GetAllWarehouseTransactionResponse;
 import com.capstone.capstone.entity.User;
 import com.capstone.capstone.entity.WarehouseItem;
 import com.capstone.capstone.entity.WarehouseTransaction;
-import com.capstone.capstone.exception.NotFoundException;
-import com.capstone.capstone.repository.EmployeeRepository;
 import com.capstone.capstone.repository.UserRepository;
 import com.capstone.capstone.repository.WarehouseItemRepository;
 import com.capstone.capstone.repository.WarehouseTransactionRepository;
 import com.capstone.capstone.service.interfaces.IWareHouseTransactionService;
 import com.capstone.capstone.util.AuthenUtil;
-import jdk.jshell.execution.Util;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -63,5 +62,32 @@ public class WareHouseTransactionService implements IWareHouseTransactionService
         response.setTransactionQuantity(transaction.getQuantity());
         response.setNote(transaction.getNote());
         return  response;
+    }
+
+    @Override
+    public List<GetAllWarehouseTransactionResponse> getAllTransactions() {
+        UUID userId = AuthenUtil.getCurrentUserId();
+        User user = userRepository.findById(userId).orElseThrow(()-> new RuntimeException("User not found"));
+        List<WarehouseTransaction> warehouseTransactions = new ArrayList<>();
+        if(user.getRole() == RoleEnum.TECHNICAL){
+            warehouseTransactions = warehouseTransactionRepository.findAllByUser(user);
+        }  else if(user.getRole() == RoleEnum.MANAGER){
+            warehouseTransactions = warehouseTransactionRepository.findAll();
+        }
+        List<GetAllWarehouseTransactionResponse> response = new ArrayList<>();
+        for (WarehouseTransaction transaction : warehouseTransactions) {
+            GetAllWarehouseTransactionResponse  res = new GetAllWarehouseTransactionResponse();
+            res.setId(transaction.getId());
+            res.setRequestId(transaction.getRequestId());
+            res.setReportId(transaction.getReportId());
+            res.setActionById(transaction.getUser().getId());
+            res.setActionByName(transaction.getUser().getFullName());
+            res.setCreatedAt(transaction.getCreatedAt());
+            res.setTransactionType(transaction.getType());
+            res.setTransactionQuantity(transaction.getQuantity());
+            res.setNote(transaction.getNote());
+            response.add(res);
+        }
+        return response;
     }
 }
