1c5ec1ca8cd815fb2b8b74d499e27dba59fd9f56
hieulmhe176232 <hieulmhe176232@fpt.edu.vn>
2025-11-24 22:14:58 +0700

    fix: update invoice

--- src/main/java/com/capstone/capstone/dto/request/invoice/UpdateInvoiceRequest.java
+++ src/main/java/com/capstone/capstone/dto/request/invoice/UpdateInvoiceRequest.java
@@ -1,9 +1,10 @@
 package com.capstone.capstone.dto.request.invoice;
 
 import com.capstone.capstone.dto.enums.PaymentStatus;
+import com.capstone.capstone.dto.response.vnpay.VNPayStatus;
 import lombok.Data;
 
 @Data
 public class UpdateInvoiceRequest {
-    private PaymentStatus status;
+    private VNPayStatus status;
 }
